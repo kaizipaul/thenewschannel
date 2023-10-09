a48243b535804d523d502a9d18de03086edda82f
kaizipaul <kaizipaul@gmail.com>
2023-10-09 16:36:44 +0300

    Add components and boilerplate code

---
+++ src/components/article-page/articlePage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ArticlePage = () => {
+  return (
+    <h2>Hello, World!</h2>
+  );
+};
+
+export default ArticlePage;
---
+++ src/components/navigation/navbar.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const navbar = () => {
+  return (
+    <h2>Hello, World!</h2>
+  );
+};
+
+export default navbar;
